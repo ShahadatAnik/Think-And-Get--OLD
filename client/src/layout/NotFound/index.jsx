a06ad20663bb45ba700c8fa853974c7f4c635815
Shahadat Anik <git.shahadat.anik@gmail.com>
2023-02-04 20:37:24 +0600

    NEW:
    - Design added
    - Base Created

---
+++ client/src/layout/NotFound/index.jsx
@@ -0,0 +1,27 @@
+export default function Index() {
+    return (
+        <section className="flex h-full items-center p-16 dark:bg-gray-900 dark:text-gray-100">
+            <div className="container mx-auto my-8 flex flex-col items-center justify-center px-5">
+                <div className="max-w-md text-center">
+                    <h2 className="mb-8 text-9xl font-extrabold dark:text-gray-600">
+                        <span className="sr-only">Error</span>404
+                    </h2>
+                    <p className="text-2xl font-semibold md:text-3xl">
+                        Sorry, we couldn't find this page.
+                    </p>
+                    <p className="mt-4 mb-8 dark:text-gray-400">
+                        But dont worry, you can find plenty of other things on
+                        our homepage.
+                    </p>
+                    <a
+                        rel="noopener noreferrer"
+                        href="/"
+                        className="rounded px-8 py-3 font-semibold dark:bg-sky-400 dark:text-gray-900"
+                    >
+                        Back to homepage
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
+}

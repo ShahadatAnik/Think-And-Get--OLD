6a2ff0f5f3057d8b5344636ff3e42b882022c30c
Shahadat Anik <git.shahadat.anik@gmail.com>
2023-02-05 01:41:01 +0600

    NEW AND FIX:
    - New page added
    - navbar added transition

--- client/src/layout/Footer/index.jsx
+++ client/src/layout/Footer/index.jsx
@@ -7,17 +7,16 @@ export default function Index() {
                 <div className="lg:w-1/3">
                     <a
                         rel="noopener noreferrer"
-                        href="#"
+                        href="/"
                         className="flex justify-center space-x-3 lg:justify-start"
                     >
-                        <div className="flex h-12 w-12 items-center justify-center rounded-full dark:bg-sky-400">
+                        <div className="flex h-12 w-12 items-center justify-center">
                             <svg
+                                className="mr-1 h-8 w-8 fill-current"
+                                viewBox="0 0 54 54"
                                 xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 32 32"
-                                fill="currentColor"
-                                className="h-5 w-5 flex-shrink-0 rounded-full dark:text-gray-900"
                             >
-                                <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
+                                <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
                             </svg>
                         </div>
                         <span className="self-center text-2xl font-semibold">
@@ -32,22 +31,22 @@ export default function Index() {
                         </h3>
                         <ul className="space-y-1">
                             <li>
-                                <a rel="noopener noreferrer" href="#">
+                                <a rel="noopener noreferrer" href="/">
                                     Features
                                 </a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">
+                                <a rel="noopener noreferrer" href="/">
                                     Integrations
                                 </a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">
+                                <a rel="noopener noreferrer" href="/">
                                     Pricing
                                 </a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">
+                                <a rel="noopener noreferrer" href="/">
                                     FAQ
                                 </a>
                             </li>
@@ -59,12 +58,12 @@ export default function Index() {
                         </h3>
                         <ul className="space-y-1">
                             <li>
-                                <a rel="noopener noreferrer" href="#">
+                                <a rel="noopener noreferrer" href="/">
                                     Privacy
                                 </a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">
+                                <a rel="noopener noreferrer" href="/">
                                     Terms of Service
                                 </a>
                             </li>
@@ -76,17 +75,17 @@ export default function Index() {
                         </h3>
                         <ul className="space-y-1">
                             <li>
-                                <a rel="noopener noreferrer" href="#">
+                                <a rel="noopener noreferrer" href="/">
                                     Public API
                                 </a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">
+                                <a rel="noopener noreferrer" href="/">
                                     Documentation
                                 </a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">
+                                <a rel="noopener noreferrer" href="/">
                                     Guides
                                 </a>
                             </li>
@@ -99,7 +98,7 @@ export default function Index() {
                         <div className="flex justify-start space-x-3">
                             <a
                                 rel="noopener noreferrer"
-                                href="#"
+                                href="/"
                                 title="Facebook"
                                 className="flex items-center p-1"
                             >
@@ -114,7 +113,7 @@ export default function Index() {
                             </a>
                             <a
                                 rel="noopener noreferrer"
-                                href="#"
+                                href="/"
                                 title="Twitter"
                                 className="flex items-center p-1"
                             >
@@ -128,7 +127,7 @@ export default function Index() {
                             </a>
                             <a
                                 rel="noopener noreferrer"
-                                href="#"
+                                href="/"
                                 title="Instagram"
                                 className="flex items-center p-1"
                             >
@@ -146,8 +145,7 @@ export default function Index() {
                 </div>
             </div>
             <div className="py-6 text-center text-sm dark:text-gray-400">
-                © {new Date().getFullYear()} Company Co. All rights
-                reserved.
+                © {new Date().getFullYear()} Company Co. All rights reserved.
             </div>
         </footer>
     );
